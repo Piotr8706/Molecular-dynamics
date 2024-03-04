dbbd422c813defb38116983106bd4f1bcbb09745
piotr8706 <piotr.beldowski.pb@gmail.com>
2024-03-04 16:00:50 +0100

    Wczytywanie pliku

--- Read_data.py
+++ Read_data.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
 """Początek"""
+
+def main():
+    file = r"C:\Users\piotr\Downloads\Results\Results\Mg_contacts_11.txt"
+    df = pd.read_csv(file, delimiter='\t')  # Assuming it's a tab-delimited file. Adjust delimiter as needed.
+
+    # Display the first few rows of the dataframe
+    print(df.head())
+    print(df.info())
+    print(df.describe())
+
+
+if __name__=='__main__':
+    main()
